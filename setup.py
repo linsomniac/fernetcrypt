f95713367f4bbe7bf9270d67d76127b92478f5e8
Sean Reifschneider <sean@realgo.com>
2023-04-15 09:38:12 -0600

    Adding long_description

--- setup.py
+++ setup.py
@@ -4,6 +4,20 @@ setup(
     name="fernetcrypt",
     version="1.0.0",
     description="FernetCrypt CLI Encrypt and decrypt files using a password.",
+    long_description="""
+A command-line tool that implements [Fernet encryption](https://cryptography.io/en/latest/fernet/).
+
+FernetCrypt encryption is a Python library that implement best-practices for encrypting
+data using a password.
+
+Fernet is a combination of AES, PKCS7, HMAC, and SHA256 for doing the heavy lifting.
+
+This tool includes a "raw" mode which just writes the raw salt and then the
+encrypted data, or the normal mode which stores the salt in base85 format and also
+includes a file identification magic string "#UF1#".  In either case, the data is
+blocked in 40,960 bytes to allow for encrypting files larger than memory.
+    """,
+    long_description_content_type='text/markdown',
     author="Sean Reifschneider",
     author_email="jafo00@gmail.com",
     url="https://github.com/linsomniac/fernetcrypt",
